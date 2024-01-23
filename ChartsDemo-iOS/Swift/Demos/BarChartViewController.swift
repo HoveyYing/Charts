03627ad113adf06f0e1c8d7e0b1b9e132092970d
HoveyYing <yingmingshun@druid.tech>
2024-01-23 19:34:35 +0800

    Update BarChartViewController.swift
    分页加载数据

--- ChartsDemo-iOS/Swift/Demos/BarChartViewController.swift
+++ ChartsDemo-iOS/Swift/Demos/BarChartViewController.swift
@@ -99,6 +99,136 @@ class BarChartViewController: DemoBaseViewController {
         sliderX.value = 12
         sliderY.value = 50
         slidersValueChanged(nil)
+        addGesture()
+    }
+    
+    func addGesture() {
+        
+        let gestureList = chartView.gestureRecognizers
+        /// 对charts自带的拖拽手势，再加一个拖拽曲线的响应事件
+        if let pan = gestureList?.first(where: { $0.isKind(of: UIPanGestureRecognizer.self) }) as? UIPanGestureRecognizer  {
+            pan.addTarget(self, action: #selector(self.handlePan))
+        }
+    }
+    
+    @objc func handlePan(_ pan: UIPanGestureRecognizer) {
+        
+        // 保证缩放在>1使用组件其自身的拖拽手势，在小于等于1时使用辅助手势相应
+        let scaleX = chartView.scaleX
+        guard scaleX <= 1 else { return }
+        
+        
+        switch pan.state {
+        case .began:
+//            chartView.gestureRecognizers?.first(where: { $0 is UITapGestureRecognizer })?.isEnabled = false
+            ()
+        case .changed:
+            
+            let translation = pan.translation(in: self.view)
+            pan.setTranslation(.zero, in: self.view)
+            
+            let pt = translation.x / self.view.bounds.width * CGFloat(Int(sliderX.value) + 1)
+            
+            let min = chartView.xAxis.axisMinimum - pt
+            let max = chartView.xAxis.axisMaximum - pt
+            
+            
+            guard let set = chartView.data?.first as? BarChartDataSet else { return }
+            
+//            guard min >= 0 else { return }
+//            guard max <= CGFloat(entrieCount + maxXVisibleCount) else { return }
+            
+            chartView.xAxis.axisMinimum = min
+            chartView.xAxis.axisMaximum = max
+            chartView.notifyDataSetChanged()
+            
+            let entrieCount = set.entries.count
+            if pt < 0 { // 左滑，拉之后
+                let crt = (min + max) / 2
+                let maxXVisibleCount = Int(sliderX.value / 4)
+                let load = entrieCount-maxXVisibleCount
+                print("crt: \(crt), load: \(load)")
+                if (load-5..<load-4).contains(Int(crt)) {
+//                if (load-2..<load-1).contains(Int(crt)) {
+                    appendChartData()
+                }
+            } else if pt > 0 {  // 右滑，拉之前
+                let crt = (min + max) / 2
+                let maxXVisibleCount = Int(sliderX.value / 4)
+                let load = maxXVisibleCount
+                print("crt: \(crt), load: \(load)")
+                
+                if ((load+3)..<(load+4)).contains(Int(crt)) {
+//                if ((load+1)..<(load+2)).contains(Int(crt)) {
+                    insertChartData()
+                }
+            }
+            
+        default:
+            ()
+        }
+    }
+    
+    func appendChartData() {
+        if self.shouldHideData {
+            chartView.data = nil
+            return
+        }
+        
+        guard let set = chartView.data?.first as? BarChartDataSet else { return }
+        
+        let start = set.entries.count
+        let count = Int(sliderX.value) + 1
+        let range = UInt32(sliderY.value)
+        (start..<start+count+1).forEach({ i in
+            let mult = range + 1
+            let val = Double(arc4random_uniform(mult)) + 10
+            let entry: BarChartDataEntry
+            entry = BarChartDataEntry(x: Double(i), y: val)
+            set.append(entry)
+        })
+        
+        print("appendChartData")
+        chartView.data?.notifyDataChanged()
+        chartView.notifyDataSetChanged()
+    }
+    
+    func insertChartData() {
+        if self.shouldHideData {
+            chartView.data = nil
+            return
+        }
+        
+        guard var set = chartView.data?.first as? BarChartDataSet else { return }
+        
+        let start = 1
+        let count = Int(sliderX.value) + 1
+        let range = UInt32(sliderY.value)
+        let yVals = (start..<start+count+1).map({ (i) -> ColorBarChartDataEntry in
+            let mult = range + 1
+            let val = Double(arc4random_uniform(mult)) + 10
+            let entry = BarChartDataEntry(x: Double(i), y: val)
+            let material = ChartColorTemplates.material()
+            let color = material[i % material.count]
+            return ColorBarChartDataEntry(color: color, entry: entry)
+        })
+        set.insert(contentsOf: yVals.map({ $0.entry }), at: 0)
+        
+        set.entries.enumerated().forEach { (index, entry) in
+            entry.x = Double(index)
+        }
+        
+        let newColors = yVals.map({ $0.color }) + set.colors
+        set.setColors(newColors, alpha: 1)
+        
+        let offset = chartView.xAxis.axisMinimum + Double(count)
+        let size = chartView.xAxis.axisMaximum - chartView.xAxis.axisMinimum
+        chartView.xAxis.axisMinimum = offset
+        chartView.xAxis.axisMaximum = offset + size
+        
+        print("insertChartData")
+        chartView.data?.notifyDataChanged()
+        chartView.notifyDataSetChanged()
     }
     
     override func updateChartData() {
@@ -113,25 +243,30 @@ class BarChartViewController: DemoBaseViewController {
     func setDataCount(_ count: Int, range: UInt32) {
         let start = 1
         
-        let yVals = (start..<start+count+1).map { (i) -> BarChartDataEntry in
+        let yVals = (start..<start+count+1).map { (i) -> ColorBarChartDataEntry in
             let mult = range + 1
-            let val = Double(arc4random_uniform(mult))
-            if arc4random_uniform(100) < 25 {
-                return BarChartDataEntry(x: Double(i), y: val, icon: UIImage(named: "icon"))
+            let val = Double(arc4random_uniform(mult)) + 10
+            let entry: BarChartDataEntry
+            if i == 3 {
+                entry = BarChartDataEntry(x: Double(i), y: val, icon: UIImage(named: "icon"))
             } else {
-                return BarChartDataEntry(x: Double(i), y: val)
+                entry = BarChartDataEntry(x: Double(i), y: val)
             }
+            let material = ChartColorTemplates.material()
+            let color = material[i % material.count]
+            return ColorBarChartDataEntry(color: color, entry: entry)
         }
         
         var set1: BarChartDataSet! = nil
         if let set = chartView.data?.first as? BarChartDataSet {
             set1 = set
-            set1.replaceEntries(yVals)
+            set1.replaceEntries(yVals.map({ $0.entry }))
             chartView.data?.notifyDataChanged()
             chartView.notifyDataSetChanged()
         } else {
-            set1 = BarChartDataSet(entries: yVals, label: "The year 2017")
-            set1.colors = ChartColorTemplates.material()
+            set1 = BarChartDataSet(entries: yVals.map({ $0.entry }), label: "The year 2017")
+//            set1.colors = ChartColorTemplates.material()
+            set1.colors = yVals.map({ $0.color })
             set1.drawValuesEnabled = false
             
             let data = BarChartData(dataSet: set1)
@@ -153,5 +288,82 @@ class BarChartViewController: DemoBaseViewController {
         sliderTextY.text = "\(Int(sliderY.value))"
         
         self.updateChartData()
+    }
+    
+    override func chartTranslated(_ chartView: ChartViewBase, dX: CGFloat, dY: CGFloat) {
+        
+//        print("dX:\(dX), dY:\(dY)")
+        let dataMax = chartView.xAxis.axisMaximum
+        
+        if (dataMax-5..<dataMax-4).contains(dX) {
+//            page += 1
+//            print("page: \(page)")
+            
+//            updateChartData()
+        }
+    }
+    
+    override func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        guard let chart = chartView as? BarChartView else { return }
+        let visibleCount = chart.visibleXRange
+        let totalDataCount = Double(chart.data?.entryCount ?? 0)
+        
+        if highlight.x == totalDataCount - visibleCount {
+            // 向右滑动（加载更老数据）
+            loadMoreData(isNewData: false)
+        } else if highlight.x == visibleCount - 1 {
+            // 向左滑动（加载新数据）
+            loadMoreData(isNewData: true)
+        }
+    }
+    
+    func chartViewDidEndPanning(_ chartView: ChartViewBase) {
+        
+        let min = chartView.xAxis.axisMinimum
+        let max = chartView.xAxis.axisMaximum
+        
+        print("DidEndPanning min:\(min), max:\(max)")
+        
+    }
+    
+    func loadMoreData(isNewData: Bool) {
+        
+        
+//        let start = 1
+//        let count = Int(sliderX.value) + 1
+//        let range = UInt32(sliderY.value)
+//
+//        let yVals = (start..<start+count+1).map { (i) -> BarChartDataEntry in
+//            let mult = range + 1
+//            let val = Double(arc4random_uniform(mult))
+//            if arc4random_uniform(100) < 25 {
+//                return BarChartDataEntry(x: Double(i), y: val, icon: UIImage(named: "icon"))
+//            } else {
+//                return BarChartDataEntry(x: Double(i), y: val)
+//            }
+//        }
+//
+//        // 示例：
+//        let moreData = yVals // 加载附加数据
+//        if isNewData {
+//            set1.append(contentsOf: moreData)
+//        } else {
+//            set1.insert(contentsOf: moreData, at: 0)
+//        }
+//
+//        // 更新图表数据
+//        chartView.data?.notifyDataChanged()
+//        chartView.notifyDataSetChanged()
+    }
+    
+}
+
+
+class ColorBarChartDataEntry {
+    let color: NSUIColor
+    let entry: BarChartDataEntry
+    init(color: UIColor, entry: BarChartDataEntry) {
+        self.color = color
+        self.entry = entry
     }
 }
